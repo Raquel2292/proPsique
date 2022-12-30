8386d9c2ebe33b7b4ca078f2cb2dc1e7a64bb943
Raquel Blázquez <raquel.blazquez.estrada@gmail.com>
2022-12-30 10:52:18 +0100

    about and pruebas home

---
+++ pages/about.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function About() {
+  return (
+
+    <div>
+        <Link href="/">
+          <h1>ProPsique</h1>
+        </Link>
+      <h1>Adriana Martín</h1>
+      <Image
+        className="fotoPerfil"
+        src="/byn.JPG"
+        alt="prueba"
+        width={250}
+        height={300}
+      />
+      
+        <p className="description">
+          Hola, soy Adriana Matín. Soy Psícologa y llevo muchos años ejerciendo.
+          Me dedico a tratar a diferentes perfiles. Puedes venir a tomar un café
+          y que solucionemos el mundo.
+        </p>
+      
+    <div className="direction">
+      <p>☎️ 630.97.64.30 </p>
+      <p>🏠 calle berrocal,3</p>
+      <p>
+      🕛 10:00-14:00 //
+        16:30-19:00
+      </p>
+    </div>
+      
+    </div>
+  );
+}
